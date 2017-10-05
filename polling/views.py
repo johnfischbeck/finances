8d17fa44d2bc4f6d13b833608cf44f3ee3ebcd8a
George Klees <george.t.klees@gmail.com>
2017-10-05 08:52:40 -0400

    Polling subapp gets fed data from backend to JS

--- polling/views.py
+++ polling/views.py
@@ -4,4 +4,17 @@ from django.shortcuts import render
 def index(request):
     """Render the index page."""
 
-    return render(request, "polling/index.html")
+    # Whether or not a race was packed - TO DO: tell this from HTTP options
+    race_picked = False
+
+    # Need to pick race
+    if not race_picked:
+        # Build a list of race options (name/year/type/state/district)
+        options = ["2004 presidential election/2004/pres/null/null"]
+        
+        # Render the page
+        print(str(options))
+        ctx = {"options":str(options)}
+        return render(request, "polling/index.html", ctx)
+    else:
+        raise Exception("Illegal state")
