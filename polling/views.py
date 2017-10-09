b3c8485bc02f2f0323b53176b8fb36f0fe86f090
George Klees <george.t.klees@gmail.com>
2017-10-09 09:13:04 -0400

    Prepare to write D3 renderer

--- polling/views.py
+++ polling/views.py
@@ -5,10 +5,11 @@ def index(request):
     """Render the index page."""
 
     # Build a list of race options (name/year/type/state/district/csv)
-    options = ["2004 presidential election/2004/pres/null/null/2004pres.csv",\
-               "2008 presidential election/2008/pres/null/null/2008pres.csv"]
+    null = None
+    options = [['2004 presidential election', '2004', 'pres', null, null, '2004pres.csv'],\
+               ['2008 presidential election', '2008', 'pres', null, null, '2008pres.csv']]
     
     # Render the page
     print(str(options))
-    ctx = {"options":str(options)}
+    ctx = {"options":str(options).replace("None","null")}
     return render(request, "polling/index.html", ctx)
