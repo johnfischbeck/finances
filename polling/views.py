a15f944e283981591bd6cc40b4f32bb55cc2c47f
George Klees <george.t.klees@gmail.com>
2017-10-09 09:06:49 -0400

    Begin the line graph display of polling data

--- polling/views.py
+++ polling/views.py
@@ -4,18 +4,11 @@ from django.shortcuts import render
 def index(request):
     """Render the index page."""
 
-    # Whether or not a race was packed - TO DO: tell this from HTTP options
-    race_picked = False
-
-    # Need to pick race
-    if not race_picked:
-        # Build a list of race options (name/year/type/state/district)
-        options = ["2004 presidential election/2004/pres/null/null",\
-                   "2008 presidential election/2008/pres/null/null"]
-        
-        # Render the page
-        print(str(options))
-        ctx = {"options":str(options)}
-        return render(request, "polling/index.html", ctx)
-    else:
-        raise Exception("Illegal state")
+    # Build a list of race options (name/year/type/state/district/csv)
+    options = ["2004 presidential election/2004/pres/null/null/2004pres.csv",\
+               "2008 presidential election/2008/pres/null/null/2008pres.csv"]
+    
+    # Render the page
+    print(str(options))
+    ctx = {"options":str(options)}
+    return render(request, "polling/index.html", ctx)
