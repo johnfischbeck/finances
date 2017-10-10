75248987937e78111597d5c1b3575921b7608ad2
Chris Wang <wangch00@icloud.com>
2017-10-10 10:54:56 -0400

    New 404 page, treemap script updates
    Signed-off-by: Chris Wang <wangch00@icloud.com>

--- static/js/treemap.js
+++ static/js/treemap.js
@@ -14,35 +14,48 @@ donation_data = [
     {"value": 1, "state": "Fl", "party": "Republican", "district": "d2"}
 ];
 
-var visualisation = d3plus.viz()
-    .container("#tmap")
-    .type("tree_map")
-    .data(donation_data)
-    .aggs("sum")
-    .id(["party", "state", "district"])
-    .size("value")
-    .messages({"branding": true})
-    .title("Donations by Party")
-    .format(
-        {
-            "text": function (text, params) {
-                if (text === "value") {
-                    return "Donation Amount, USD"
+try {
+    var visualisation = d3plus.viz()
+        .container("#tmap")
+        .type("tree_map")
+        .data(donation_data)
+        .aggs("sum")
+        .id(["party", "state", "district"])
+        .size("value")
+        .messages({"branding": true})
+        .title({"value": "Donations by Party", "font": {"size": 21}})
+        .format(
+            {
+                "text": function (text, params) {
+                    if (text === "value") {
+                        return "Donation Amount, USD"
+                    }
+                    else {
+                        return d3plus.string.title(text, params)
+                    }
+                },
+                "number": function (number, params) {
+                    var formatted = d3plus.number.format(number, params);
+                    if (params.key === "value") {
+                        return "$" + formatted + " USD"
+                    }
+                    else {
+                        return formatted;
+                    }
                 }
-                else {
-                    return d3plus.string.title(text, params)
-                }
-            },
-            "number": {
-
             }
-        }
-    )
-    /*.time({
-        "value": "Year",
-        "solo": 2012
-    })*/
-    .draw();
+        )
+        .ui(
+            [{"method":"color", "value": ["value", "state", "party", "district"]}]
+        )
+        /*.time({
+            "value": "Year",
+            "solo": 2012
+        })*/
+        .draw();
+} catch (e) {
+    document.getElementById("tmap").innerHTML = "<div style='text-align: center'><h1>Oops! Something had a sad...</h1> <br/> <p>Go back to the <a href='../' >home?</a></p></div>"
+}
 
 function download(url) {
     return new Promise(function (resolve, reject) {
