0fc42d5066f6233474dad5facb934d9d428bea8b
Chris Wang <kisa.ayano@icloud.com>
2017-10-10 09:16:52 -0400

    Progress on treemap, add fake data for testing
    Signed-off-by: Chris Wang <wangch00@icloud.com>

--- static/js/treemap.js
+++ static/js/treemap.js
@@ -3,10 +3,55 @@ const HMROOT = "/static/data/heatmap";
 const STATES = HMROOT + "/states.json";
 const DISTRICTS = HMROOT + "/cds.json";
 
+/*var donation_data = download("eastwatch.mbhs.edu/data/aggregate/district");*/
+
+donation_data = [
+    {"value": 100, "state": "Al", "party": "Republican", "district": "d1"},
+    {"value": 70, "state": "Al", "party": "Democratic", "district": "d2"},
+    {"value": 40, "state": "Ga", "party": "Democratic", "district": "d1"},
+    {"value": 15, "state": "Ga", "party": "Democratic", "district": "d3"},
+    {"value": 5, "state": "Fl", "party": "Republican", "district": "d9"},
+    {"value": 1, "state": "Fl", "party": "Republican", "district": "d2"}
+];
+
 var visualisation = d3plus.viz()
     .container("#tmap")
     .type("tree_map")
-    .id(["party","state","district",])
+    .data(donation_data)
+    .aggs("sum")
+    .id(["party", "state", "district"])
     .size("value")
-    .draw()
+    .messages({"branding": true})
+    .title("Donations by Party")
+    .format(
+        {
+            "text": function (text, params) {
+                if (text === "value") {
+                    return "Donation Amount, USD"
+                }
+                else {
+                    return d3plus.string.title(text, params)
+                }
+            },
+            "number": {
+
+            }
+        }
+    )
+    /*.time({
+        "value": "Year",
+        "solo": 2012
+    })*/
+    .draw();
+
+function download(url) {
+    return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        request.addEventListener("load", function () {
+            resolve(JSON.parse(request.responseText));
+        });
+        request.open("GET", url);
+        request.send();
+    });
+}
 
