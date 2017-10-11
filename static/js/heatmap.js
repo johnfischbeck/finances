7261210e36a01bb1a751d8dbcabdd4c46160c522
Noah Kim <noahbkim@gmail.com>
2017-10-11 16:48:48 -0400

    Started on actual view for finance

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,4 +1,7 @@
-var width = 960, height = 500, centered;
+var width = window.innerWidth - 400;
+var height = window.innerHeight;
+var centered;
+
 
 /* Create the projection of the US map. */
 var projection = d3.geo.albersUsa().scale(1070).translate([width / 2, height / 2]);
@@ -10,7 +13,7 @@ var svg = d3.select("body").append("svg")
   .attr("height", height);
 
 /* Create the background to support zoom click-out. */
-svg.append("rect")
+var background = svg.append("rect")
   .attr("class", "background")
   .attr("width", width)
   .attr("height", height)
@@ -22,7 +25,7 @@ var g = svg.append("g");
 /* Queue assets. */
 queue()
   .defer(d3.json, "/static/data/heatmap/us.json")
-  .defer(d3.json, "/static/data/heatmap/us-congress-113.json")
+  .defer(d3.json, "/static/data/heatmap/congress.json")
   .await(ready);
 
 
@@ -39,10 +42,11 @@ function ready(error, us, congress) {
       .on("click", clicked);
 
   /* Draw the separation between states. */
-  var path = g.append("path");
-  path.datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+  var stateBorder = g.append("path");
+  stateBorder.datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
     .attr("id", "state-borders")
     .attr("d", path);
+
 }
 
 function clicked(d) {
@@ -62,65 +66,10 @@ function clicked(d) {
   }
 
   g.selectAll("path")
-      .classed("active", centered && function(d) { return d === centered; });
+    .classed("active", centered && function(d) { return d === centered; });
 
   g.transition()
-      .duration(750)
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-      .style("stroke-width", 1.5 / k + "px");
+    .duration(750)
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+    .style("stroke-width", 1.5 / k + "px");
 }
-
-/* var width = 960,
-    height = 600;
-
-var projection = d3.geo.albersUsa()
-    .scale(1280)
-    .translate([width / 2, height / 2]);
-
-var path = d3.geo.path()
-    .projection(projection);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-queue()
-    .defer(d3.json, "/static/data/heatmap/us.json")
-    .defer(d3.json, "/static/data/heatmap/us-congress-113.json")
-    .await(ready);
-
-function ready(error, us, congress) {
-  if (error) throw error;
-
-  svg.append("defs").append("path")
-      .attr("id", "land")
-      .datum(topojson.feature(us, us.objects.land))
-      .attr("d", path);
-
-  svg.append("clipPath")
-      .attr("id", "clip-land")
-    .append("use")
-      .attr("xlink:href", "#land");
-
-  svg.append("g")
-      .attr("class", "districts")
-      .attr("clip-path", "url(#clip-land)")
-    .selectAll("path")
-      .data(topojson.feature(congress, congress.objects.districts).features)
-    .enter().append("path")
-      .attr("d", path)
-    .append("title")
-      .text(function(d) { return d.id; });
-
-  svg.append("path")
-      .attr("class", "district-boundaries")
-      .datum(topojson.mesh(congress, congress.objects.districts, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
-      .attr("d", path);
-
-  svg.append("path")
-      .attr("class", "state-boundaries")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("d", path);
-}
-
-d3.select(self.frameElement).style("height", height + "px"); */
