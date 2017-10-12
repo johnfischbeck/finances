adbced7c9d15ff2eb812dfe7db8f6d5589bca6dc
Artemis Tosini <me@artemis.re>
2017-10-12 08:33:03 -0400

    Improved error handling in individual importer

--- shared/management/commands/import_indiv.py
+++ shared/management/commands/import_indiv.py
@@ -15,6 +15,7 @@ class Command(BaseCommand):
         outside_name = 'indiv'
         inside_name = 'itcont.txt'
         years = [i for i in os.listdir(data_dir) if fullmatch('[\d]{4}', i)]
+        error_file = open("errors.txt", "w")
 
 
         for year in years:
@@ -25,15 +26,25 @@ class Command(BaseCommand):
                 continue
             with zf.open(inside_name) as inner_file:
                 print("opened data for {}".format(year))
-                rows = 0
-                reader = csv.reader(codecs.iterdecode(inner_file, 'utf8'), delimiter='|')
+                rows = errors = 0
+                reader = csv.reader(codecs.iterdecode(inner_file, 'utf8', errors="ignore"), delimiter='|')
                 for row in reader:
-                    row = list(row)
-                    row[13] = row[13][-4:] + '-' + row[13][:2] + '-' + row[13][2:-4]
-                    row[15] = row[15] or None
-                    row[17] = row[17] or None
-                    if Candidate.objects.filter(cand_pcc=row[0]).exists():
-                        IndivCandDonation.objects.create(**dict(zip(names, row)))
-                    rows+= 1
-                if rows % 1000 == 0:
-                    print("{}: {} rows loaded".format(year, rows))
+                    try:
+                        row = list(row)
+                        if len(row) != 21:
+                            print("Invalid row length")
+                            continue
+                        if row[13]:
+                            row[13] = row[13][-4:] + '-' + row[13][:2] + '-' + row[13][2:-4]
+                        row[15] = row[15] or None
+                        row[17] = row[17] or None
+                        if Candidate.objects.filter(cand_pcc=row[0]).exists() and not IndivCandDonation.objects.filter(row_id=row[-1]):
+                            IndivCandDonation.objects.create(**dict(zip(names, row)))
+                        rows += 1
+                    except Exception:
+                        print("Row failed: transaction id {}".format(row[-1]))
+                        error_file.write(str(row) + '\n')
+                        errors += 1
+                        if errors % 10 == 0:
+                            error_file.flush()
+                        
