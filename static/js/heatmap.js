289c432e78e2b3cd919a7244eb5fd7712e1dc5de
Noah Kim <noahbkim@gmail.com>
2017-10-17 08:59:46 -0400

    Weird issues with ordering of states in geojson

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -2,6 +2,8 @@ var width = window.innerWidth - 400;
 var height = window.innerHeight;
 var centered;
 
+//            1                                      4
+var states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 
 /* Create the projection of the US map. */
 var projection = d3.geo.albersUsa().scale(1070).translate([width / 2, height / 2]);
@@ -51,6 +53,8 @@ function ready(error, us, congress) {
 
 function clicked(d) {
   var x, y, k;
+
+  console.log(d.id);
 
   if (d && centered !== d) {
     var centroid = path.centroid(d);
