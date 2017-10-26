fc967e0e34c1c4b2662c40defad0f5a6625cbd56
Artemis Tosini <me@artemis.re>
2017-10-26 13:09:18 +0000

    Add initial heatmap data

--- heatmap/views.py
+++ heatmap/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     """Render the index page."""
 
     return render(request, "heatmap/index.html")
+
+def data(request):
+    # TODO: Add filtering
+    return redirect('/static/data/heatmap/heatmap.json')
