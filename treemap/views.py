5686f74adca102e7f42fc7739796de2107bb776c
Artemis Tosini <me@artemis.re>
2017-10-27 01:40:30 +0000

    add treemap data

--- treemap/views.py
+++ treemap/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     """Render the index page."""
 
     return render(request, "treemap/index.html")
+
+
+def data(request):
+    return redirect('/static/data/treemap.csv')
