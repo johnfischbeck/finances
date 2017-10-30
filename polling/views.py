c73d401be8600c741887290bb96fca47fc3c2966
Artemis Tosini <me@artemis.re>
2017-10-30 12:13:33 +0000

    Add data for polling

--- polling/views.py
+++ polling/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.db import connection
+import csv
 
-
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """Render the index page."""
 
     # Build a list of race options (year/type/state/district/csv)
@@ -13,3 +15,27 @@ def index(request):
     print(str(options))
     ctx = {"options":str(options).replace("None","null")}
     return render(request, "polling/index.html", ctx)
+
+
+def data(request: HttpRequest) -> HttpResponse:
+    """
+    Return data for the polling application
+    """
+    try:
+        office = request.GET['office']
+        state = 'US' if office == 'P' else request.GET['state']
+        district = '00' if office == 'P' else request.GET['district']
+        year = request.GET['year']
+    except KeyError:
+        return HttpResponse(return_code=404)
+    with connection.cursor() as cursor:
+        query_result = cursor.execute('SELECT date, sum, name FROM histogram_data WHERE year=%d AND office=%s AND state=%s AND district=%s;', [year, office, state, district])
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+    writer = csv.writer(response)
+    writer.writerows(query_result)
+    
+    return response
+
