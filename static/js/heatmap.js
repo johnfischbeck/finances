cf2ab5bae5d08423939a855f1b2e1be9a8bbd041
Noah Kim <noahbkim@gmail.com>
2017-10-30 22:36:56 -0400

    Cleaned up code

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -2,18 +2,26 @@ const width = window.innerWidth - 349;
 const height = window.innerHeight;
 let centered;
 
+
+
+
 const CURRENT = "2017";
 const OVERALL = "Overall";
 const DONATION_AMOUNT = "Donation amount";
 const DONATION_AMOUNT_BY_PARTY = "Donation amount by party";
 const GRADIENT_BY_PARTY = "Gradient by party";
 
-const stateNames =         ["", "Alabama", "Alaska", "?", "Arizona", "Arkansas", "California", "?", "Colorado", "Connecticut", "Delaware", "?", "Florida", "Georgia", "?", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "?", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "?", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-const stateAbbreviations = ["", "AL",      "AK",     "?", "AZ",      "AR",       "CA",         "?", "CO",       "CT",          "DE",       "?", "FL",      "GA",      "?", "HI",     "ID",    "IL",       "IN",      "IA",   "KS",     "KY",       "LA",        "ME",    "MD",       "MA",            "MI",       "MN",        "MS",          "MO",       "MT",      "NE",       "NV",     "NH",            "NJ",         "NM",         "NY",       "NC",             "ND",           "OH",   "OK",       "OR",     "PA",           "?", "RI",           "SC",             "SD",           "TN",        "TX",    "UT",   "VT",      "VA",       "?", "WA",         "WV",            "WI",        "WY"];
-
-const statePaths = {};
+const state = {
+  names:         ["?", "Alabama", "Alaska", "?", "Arizona", "Arkansas", "California", "?", "Colorado", "Connecticut", "Delaware", "?", "Florida", "Georgia", "?", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "?", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "?", "Washington", "West Virginia", "Wisconsin", "Wyoming"],
+  abbreviations: ["?", "AL",      "AK",     "?", "AZ",      "AR",       "CA",         "?", "CO",       "CT",          "DE",       "?", "FL",      "GA",      "?", "HI",     "ID",    "IL",       "IN",      "IA",   "KS",     "KY",       "LA",        "ME",    "MD",       "MA",            "MI",       "MN",        "MS",          "MO",       "MT",      "NE",       "NV",     "NH",            "NJ",         "NM",         "NY",       "NC",             "ND",           "OH",   "OK",       "OR",     "PA",           "?", "RI",           "SC",             "SD",           "TN",        "TX",    "UT",   "VT",      "VA",       "?", "WA",         "WV",            "WI",        "WY"],
+  ids: [],
+  paths: {},
+};
 const mode = {year: CURRENT, mode: DONATION_AMOUNT};
-const donations = {};
+const data = {donations: {}, population: {}};
+for (let i = 0; i < state.names.length; i++) { if (state.names[i] !== "?") state.ids.push(i); }
+
+
 
 /* Create the projection of the US map. */
 let projection = d3.geo.albersUsa().scale(1080).translate([width / 2, height / 2]);
@@ -38,11 +46,10 @@ let g = svg.append("g");
 queue()
   .defer(d3.json, "/static/data/heatmap/us.json")
   .defer(d3.json, "/static/data/heatmap/congress.json")
-  .await(ready);
-
+  .await(onShapes);
 
 /** Called when the shapefile download is ready. */
-function ready(error, us, congress) {
+function onShapes(error, us, congress) {
   if (error) throw error;
 
   g.append("path")
@@ -50,13 +57,13 @@ function ready(error, us, congress) {
     .attr("id", "land")
     .attr("d", path);
 
-  /* Draw the border between stateNames. */
+  /* Draw the border between state.names. */
   let stateBorder = g.append("path");
   stateBorder.datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
     .attr("id", "state-borders")
     .attr("d", path);
 
-  /* Draw each individual stateNames. */
+  /* Draw each individual state.names. */
   let states = g.append("g").attr("id", "states");
   states.selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
@@ -66,10 +73,10 @@ function ready(error, us, congress) {
       .on("click", clicked)
       .call(function() {
         let statesObject = document.getElementById("states");
-        for (let i = 1; i < stateNames.length; i++) {
-          for (let state of statesObject.children) {
-            if (+state.id === i) {
-              statePaths[i] = state;
+        for (let stateId of state.ids) {
+          for (let statePath of statesObject.children) {
+            if (+statePath.id === stateId) {
+              state.paths[stateId] = statePath;
               break;
             }
           }
@@ -85,68 +92,86 @@ function ready(error, us, congress) {
 
 }
 
-d3.tsv("/static/data/treemap.tsv", function(error, data) {
-  data.forEach(function(d) {
-    let id = stateAbbreviations.indexOf(d.STATE);
-    if (typeof donations[id] !== "object") donations[id] = [];
-    donations[id].push({
+
+queue()
+  .defer(d3.tsv, "/static/data/treemap.tsv")
+  .await(onData);
+
+function onData(error, donations) {
+
+  data.donations.raw = {};
+  donations.forEach(function(d) {
+    let id = state.abbreviations.indexOf(d.STATE);
+    if (typeof data.donations.raw[id] !== "object") data.donations.raw[id] = [];
+    data.donations.raw[id].push({
       amount: +d.AMT,
       party: d.PARTY,
       year: +d.YEAR
     });
   });
-});
+
+  data.donations.current = {min: null, max: null};
+  data.donations.overall = {min: null, max: null};
+  for (let stateId of state.ids) {
+    let currentTotal = 0;
+    let overallTotal = 0;
+    for (let donation of data.donations.raw[stateId]) {
+      if (donation.year === 2017) currentTotal += donation.amount;
+      overallTotal += donation.amount;
+    }
+    if (data.donations.current.min === null || currentTotal < data.donations.current.min)
+      data.donations.current.min = currentTotal;
+    if (data.donations.current.max === null || currentTotal > data.donations.current.max)
+      data.donations.current.max = currentTotal;
+    if (data.donations.overall.min === null || overallTotal < data.donations.overall.min)
+      data.donations.overall.min = overallTotal;
+    if (data.donations.overall.max === null || overallTotal < data.donations.current.max)
+      data.donations.overall.max = overallTotal;
+    data.donations.current[stateId] = currentTotal;
+    data.donations.overall[stateId] = overallTotal;
+  }
+
+}
 
 
 function fillStates() {
   if (mode.mode === DONATION_AMOUNT) {
-    let totals = {};
-    let min = null;
-    let max = null;
-    for (let i in donations) {
-      let total = 0;
-      for (let j = 0; j < donations[i].length; j++) {
-        if (mode.year === OVERALL || donations[i][j].year === 2017) {
-          total += donations[i][j].amount;
-        }
+    if (mode.year === CURRENT) {
+      let min = data.donations.current.min, max = data.donations.current.max;
+      for (let stateId of state.ids) {
+        if (stateId === "min" || stateId === "max") continue;
+        let total = data.donations.current[stateId];
+        state.paths[stateId].style.fill = "rgba(0, 0, 255, " + (total - min) / max + ")";
       }
-      totals[i] = total;
-      if (min === null || total < min) min = total;
-      if (max === null || total > max) max = total;
     }
-    for (let i in totals) {
-      if (i == -1) continue;
-      statePaths[i].style.fill = "rgba(0, 0, 255, " + (totals[i] - min) / max + ")";
-    }
-
   }
 }
 
 
 /* Gather HTML elements. */
 const $ = function(id) { return document.getElementById(id); };
-const country = $("sidebar-country");
-const state = $("sidebar-state");
+const countrySidebar = $("sidebar-country");
+const stateSidebar = $("sidebar-state");
 const e = {
   name: $("state-name")
 };
 
 /* Load sidebar information. */
 function loadSidebar(id) {
-  country.style.display = "none";
-  state.style.display = "block";
+  countrySidebar.style.display = "none";
+  stateSidebar.style.display = "block";
 
-  const name = stateNames[id];
+  let name = state.names[id];
   e.name.innerHTML = name;
 }
 
 function clearSidebar() {
-  state.style.display = "none";
-  country.style.display = "block";
+  stateSidebar.style.display = "none";
+  countrySidebar.style.display = "block";
 }
 
 /* Display state sidebar. */
-country.style.display = "block";
+countrySidebar.style.display = "block";
 
 
 function clicked(d) {
