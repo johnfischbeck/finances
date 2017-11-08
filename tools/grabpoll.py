cf4103262ace25bcaa48225e974c9ef35b604eb1
George Klees <george.t.klees@gmail.com>
2017-11-07 20:28:23 -0500

    Making good progress on script

--- tools/grabpoll.py
+++ tools/grabpoll.py
@@ -39,19 +39,36 @@ if col_dem == -1:
     print("Bad CSV")
     sys.exit(1)
 
-col_dem = -1
+col_rep = -1
 for i in range(0, len(valid_rows[0])):
     opt = valid_rows[0][i]
-    if "(D)" in opt:
-        col_dem = i
+    if "(R)" in opt:
+        col_rep = i
         break
-if col_dem == -1:
+if col_rep == -1:
     print("Bad CSV")
     sys.exit(1)
 
 # Start building a running average
 final_rows = []
 
+running_sum_dem = 0
+running_sum_rep = 0
+n = 0
+
 cur_row = len(valid_rows) - 1
 while cur_row > 0:
-    pass
+    # If n == 5, we need to eliminate the oldest
+    if n == 5:
+        running_sum_dem -= valid_rows[cur_row-5][col_dem]
+        running_sum_rep -= valid_rows[cur_row-5][col_rep]
+    
+    # Add current row to our running total
+    running_sum_dem += valid_rows[cur_row][col_dem]
+    running_sum_rep += valid_rows[cur_row][col_rep]
+
+    # Now increase our n if it's less than 5
+    if n < 5:
+        n += 1
+
+    # 
